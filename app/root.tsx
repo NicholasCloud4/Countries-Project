135aec10d26d7b4df805a32750497da3f7135db5
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-01-31 09:50:41 -0500

    Worked on Navbar component

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import Navbar from "./components/navbar";
 
 export const links: Route.LinksFunction = () => [
     { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -45,10 +46,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-    return;
-    <div>
-        <Outlet />
-    </div>;
+    return (
+        <div>
+            <Navbar />
+            <Outlet />
+        </div>
+    );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
